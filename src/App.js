227310cf3a14d2b616ac314801e2b7ef013ff399
ThomasPellegrini <pellegrinithomas1@gmail.com>
2021-09-01 13:54:24 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
 
+//TEST
+
 function App() {
   return (
     <SortingVisualizer></SortingVisualizer>
